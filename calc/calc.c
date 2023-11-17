17bb1e6d5107c200ea9e3a52c6d1df0d59e2611d
Adriano Cahete <me@adrianocahete.dev>
2023-11-17 13:38:46 -0300

    feat(app): add a simple base calc

--- calc/calc.c
+++ calc/calc.c
@@ -3,6 +3,37 @@
 int main()
 {
 
-    // Vars dos inputs e result
-    float n1, n2, res;
+    char op;
+    int n1, n2;
+    float res;
+
+    printf("+, -, *, /");
+    scanf("%c", &op);
+
+    scanf(" %d", &n1);
+    scanf(" %d", &n2);
+
+    switch (op)
+    {
+    case '+':
+        res = n1 + n2;
+        printf("%d + %d e %.2f", n1, n2, res);
+        break;
+
+    case '-':
+        res = n1 - n2;
+        printf("%d - %d e %.2f", n1, n2, res);
+        break;
+
+    case '*':
+        res = n1 * n2;
+        printf("%d x %d e %.2f", n1, n2, res);
+        break;
+
+    case '/':
+        res = n1 / n2;
+        printf(" %d / por %d e %.2f", n1, n2, res);
+        break;
+        return 0;
+    }
 }
