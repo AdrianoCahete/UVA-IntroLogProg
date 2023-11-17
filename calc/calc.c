21cf4822e790bc484b84bd7a00589eeca4254c5c
Adriano Cahete <me@adrianocahete.dev>
2023-11-17 14:03:31 -0300

    feat(app): fix return and add todos to make before lunch

--- calc/calc.c
+++ calc/calc.c
@@ -1,3 +1,9 @@
+// College work for UVA 2023
+// by Adriano Cahete -- https://github.com/AdrianoCahete
+//
+// Yes, I put all my code and comments in english, sorry about that.
+// And, please, do not copy or your work will be flagged as plagiarism.
+
 #include <stdio.h>
 
 int main()
@@ -8,10 +14,15 @@ int main()
     float res;
 
     printf("+, -, *, /");
+
     scanf("%c", &op);
+    // TODO: Make selectable instead of wait for input
 
     scanf(" %d", &n1);
     scanf(" %d", &n2);
+    // TODO: Check if is a number
+
+    // TODO: Show chosen op
 
     switch (op)
     {
@@ -32,8 +43,10 @@ int main()
 
     case '/':
         res = n1 / n2;
+        // TODO: Check if 0
         printf(" %d / por %d e %.2f", n1, n2, res);
         break;
-        return 0;
     }
+
+    return 0;
 }
