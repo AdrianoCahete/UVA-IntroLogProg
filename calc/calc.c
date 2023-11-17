b26bb2e34b2df8c30bc66ec572ad59fe36f14708
Adriano Cahete <me@adrianocahete.dev>
2023-11-17 17:07:54 -0300

    feat(app): add more statuses + fix secondary cases

--- calc/calc.c
+++ calc/calc.c
@@ -13,58 +13,64 @@ int main()
     int n1, n2;
     float res;
 
-    clrscr();
-
-    printf("Escolha uma operação mantemática: +, -, *, / \n");
+    printf("Escolha uma operacao matematica, usando os simbolos de cada uma: \n");
     printf("1 [+]: Soma \n");
-    printf("2 [/]: Subtração \n");
-    printf("2 [/]: Multiplicação \n");
-    printf("2 [/]: Divisão \n");
+    printf("2 [-]: Subtracao \n");
+    printf("3 [*]: Multiplicacao \n");
+    printf("4 [/]: Divisao \n\n");
 
     scanf("%c", &op);
 
     if (op == '+' || op == '1')
     {
-        printf("[+] Você selecionou Soma");
+        printf("[+] Voce selecionou Soma \n");
     }
     else if (op == '-' || op == '2')
     {
-        printf("[-] Você selecionou Subtração");
+        printf("[-] Voce selecionou Subtracao \n");
     }
     else if (op == '*' || op == '3')
     {
-        printf("[/] Você selecionou Multiplicação");
+        printf("[*] Voce selecionou Multiplicacao\n");
     }
     else if (op == '/' || op == '4')
     {
-        printf("[/] Você selecionou Divisão");
+        printf("[/] Voce selecionou Divisao\n");
     }
 
-    scanf(" %d", &n1);
-    scanf(" %d", &n2);
+    printf("    Digite o primeiro numero: ");
+    scanf("%d", &n1);
+
+    printf("    Digite o segundo numero: ");
+    scanf("%d", &n2);
+
     // TODO: Check if is a number
 
     switch (op)
     {
     case '+':
+    case '1':
         res = n1 + n2;
-        printf("%d + %d e %.2f", n1, n2, res);
+        printf("\n[=] A soma de %d mais %d e igual a %.2f \n\n----------\n", n1, n2, res);
         break;
 
     case '-':
+    case '2':
         res = n1 - n2;
-        printf("%d - %d e %.2f", n1, n2, res);
+        printf("\n[=] A subtracao de %d menos %d e igual a %.2f \n\n----------\n", n1, n2, res);
         break;
 
     case '*':
+    case '3':
         res = n1 * n2;
-        printf("%d x %d e %.2f", n1, n2, res);
+        printf("\n[=] A multiplicacao de %d por %d e igual a %.2f \n\n----------\n", n1, n2, res);
         break;
 
     case '/':
+    case '4':
         res = n1 / n2;
         // TODO: Check if 0
-        printf(" %d / por %d e %.2f", n1, n2, res);
+        printf("\n[=] A divisao de %d por %d e igual a %.2f \n\n----------\n", n1, n2, res);
         break;
     }
 
