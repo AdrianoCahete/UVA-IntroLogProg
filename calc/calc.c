8d0cd8b5fade4dbaed9c2a855eabb4f874778870
Adriano Cahete <me@adrianocahete.dev>
2023-11-17 18:12:00 -0300

    feat(app): add copyright info to header

--- calc/calc.c
+++ calc/calc.c
@@ -1,8 +1,10 @@
 // College work for UVA 2023
-// by Adriano Cahete -- https://github.com/AdrianoCahete
+// by Adriano Cahete -- https://github.com/AdrianoCahete / https://adrianocahete.dev/
+//
+// Source: https://github.com/AdrianoCahete/UVA-IntroLogProg
 //
 // Yes, I put all my code and comments in english, sorry about that.
-// And, please, do not copy or your work will be flagged as plagiarism.
+// To students: please, do not copy or your work will be flagged as plagiarism.
 
 #include <stdio.h>
 
