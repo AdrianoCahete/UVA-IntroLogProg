d57d71b6467d72d5e4c76b013153a1d235824a33
Adriano Cahete <me@adrianocahete.dev>
2023-11-17 18:26:19 -0300

    feat(app): add ava2 intro

---
+++ evenodd/evenodd.c
@@ -0,0 +1,13 @@
+// College work for UVA 2023
+// by Adriano Cahete -- https://github.com/AdrianoCahete / https://adrianocahete.dev/
+//
+// Source: https://github.com/AdrianoCahete/UVA-IntroLogProg
+//
+// Yes, I put all my code and comments in english, sorry about that.
+// To students: please, do not copy or your work will be flagged as plagiarism.
+
+#include <stdio.h>
+
+int main()
+{
+}
