3a1dd4ccaf6b87d2953b72ee15469d91fd2bf21e
Adriano Cahete <me@adrianocahete.dev>
2023-11-24 20:37:06 -0300

    feat(app): start matrix av2

---
+++ matrix/matrix.c
@@ -0,0 +1,15 @@
+// College work for UVA 2023
+// by Adriano Cahete -- https://github.com/AdrianoCahete / https://adrianocahete.dev/
+//
+// Source: https://github.com/AdrianoCahete/UVA-IntroLogProg/matrix
+//
+// I will use the same repo for everything, sorry for bad naming at the time. :(
+//
+// Yes, I put all my code and comments in english, sorry about that.
+// To students: please, do not copy or your work will be flagged as plagiarism.
+
+#include <stdio.h>
+
+int main()
+{
+}
