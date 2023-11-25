3be91f74c5adbf97cb4c06566080397b7b92d7f8
Adriano Cahete <me@adrianocahete.dev>
2023-11-24 23:34:32 -0300

    feat(app): fixing source url

--- matrix/matrix.c
+++ matrix/matrix.c
@@ -1,7 +1,7 @@
 // College work for UVA 2023
 // by Adriano Cahete -- https://github.com/AdrianoCahete / https://adrianocahete.dev/
 //
-// Source: https://github.com/AdrianoCahete/UVA-IntroLogProg/matrix
+// Source: https://github.com/AdrianoCahete/UVA-IntroLogProg/tree/main/matrix
 //
 // I will use the same repo for everything, sorry for bad naming at the time. :(
 //
